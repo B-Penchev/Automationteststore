eb35686d82891243b38483c124c6d477e3f656ff
B-Penchev <penchev2086@gmail.com>
2026-04-26 20:55:45 +0300

    Add Special Tests

---
+++ Tests/SpecialTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+
+namespace Tests;
+
+public class SpecialsTests : BaseTest
+{
+    [Fact]
+    public void SpecialsPage_AllProducts_DisplayDiscountedPrice()
+    {
+        var specialsPage = HomePage
+            .ClickLoginOrRegister()
+            .Login(Settings.ExistingUser.LoginName, Settings.ExistingUser.Password)
+            .ClickSpecials();
+
+        var salePricePresence = specialsPage.GetSalePricePresencePerProduct();
+
+        salePricePresence.Should().NotBeEmpty(
+            because: "the Specials page should always have at least one product");
+
+        salePricePresence.Should().AllSatisfy(hasPrice =>
+            hasPrice.Should().BeTrue(
+                because: "every product on the Specials page must display a discounted price"));
+    }
+}
